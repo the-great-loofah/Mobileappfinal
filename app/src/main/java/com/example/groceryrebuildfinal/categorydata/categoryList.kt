2819daa33d9c4ffb6a34cee4ebc1dc7211dfac30
GreatLoofah <uzayr34@hotmail.ca>
2026-04-24 23:19:33 -0600

    changed read only list types to val, minor cleanup

--- app/src/main/java/com/example/groceryrebuildfinal/categorydata/categoryList.kt
+++ app/src/main/java/com/example/groceryrebuildfinal/categorydata/categoryList.kt
@@ -5,13 +5,13 @@ import android.R
 //I understand this is bad :(
 
 //Collection of category titles and some sample items in them
-//These are vars so if I figure out how to get the user to add to them, theyre already read/write
-var fruitsList: List<String> = listOf("Apples", "Oranges", "StrawBerry", "Grapes", "Pineapple") //Might add this to the string.xml but the sheer quantity of items makes it excessive despite the efficiency gains
 
-var vegetableList: List<String> = listOf("Broccoli", "Potatoes", "Lettuce", "Onions") //The only 4 vegetables youll even need
+val fruitsList: List<String> = listOf("Apples", "Oranges", "StrawBerry", "Grapes", "Pineapple") //Might add this to the string.xml but the sheer quantity of items makes it excessive despite the efficiency gains
 
-var meatList: List<String> = listOf("Beef", "Chicken", "Tuna", "Salmon") //ordered in terms of healthiness from worst to best (source?: It came to me in a dream)
+val vegetableList: List<String> = listOf("Broccoli", "Potatoes", "Lettuce", "Onions") //The only 4 vegetables youll even need
 
-var dairyList: List<String> = listOf("Milk", "Butter", "Eggs", "Cheese") //♫One of these things are not like the others♫
+val meatList: List<String> = listOf("Beef", "Chicken", "Tuna", "Salmon") //ordered in terms of healthiness from worst to best (source?: It came to me in a dream)
+
+val dairyList: List<String> = listOf("Milk", "Butter", "Eggs", "Cheese") //♫One of these things are not like the others♫
 
 var ohAnd: MutableList<String> = mutableListOf()
